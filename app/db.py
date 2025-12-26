335afd5dd62a590d5988a03bac9ed0dce12c6821
Janaki Rajvir <janakirajvir08@gmail.com>
2025-12-26 10:18:21 +0530

    Delete create_expense function
    Remove create_expense function from db.py

--- app/db.py
+++ app/db.py
@@ -1,10 +1,1 @@
-from models import Expense
-from schemas import ExpenseCreate
-from sqlalchemy.orm import Session
 
-def create_expense(db: Session, data: ExpenseCreate):
-    expense_instance = Expense(**data.model_dump())
-    db.add(expense_instance)
-    db.commit()
-    db.refresh(expense_instance)
-    return expense_instance
